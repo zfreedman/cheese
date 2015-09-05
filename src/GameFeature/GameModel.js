3d61be91885eebc735482e5579e20e9062c71a80
zfreedman <zfreedman92@gmail.com>
2015-09-05 14:48:55 -0700

    Fixed maze implementation to clearly print maze and also display user starting position for print function

--- src/GameFeature/GameModel.js
+++ src/GameFeature/GameModel.js
@@ -2,6 +2,6 @@ var GameModel = Backbone.Model.extend({
 	// Initialize function
 	initialize: function(){
 		//Maze model
-		this.set("mazeModel", new MazeModel({n: 9}));
+		this.set("mazeModel", new MazeModel({n: 13}));
 	}
 });
--- src/MazeFeature/MazeModel.js
+++ src/MazeFeature/MazeModel.js
@@ -25,9 +25,9 @@ var MazeModel = Backbone.Model.extend({
 			//Push new row
 			this.get("maze").push([]);
 			//If the is the first or last row
-			if(i === 0 || i === (this.get("n") - 1)){
+			if(i % 2 === 0){
 				for(var j = 0; j < this.get("n"); ++j){
-					this.get("maze")[i].push(1);
+					this.get("maze")[i].push('-');
 				}
 				continue;
 			}
@@ -35,11 +35,11 @@ var MazeModel = Backbone.Model.extend({
 			for(var j = 0; j < this.get("n"); ++j){
 				//If the index is even, put a wall
 				if(j % 2 === 0){
-					this.get("maze")[i].push(1);
+					this.get("maze")[i].push('|');
 				}
 				//Otherwise, push empty space
 				else{
-					this.get("maze")[i].push(0);
+					this.get("maze")[i].push(1);
 				}
 			}
 		}
@@ -55,9 +55,11 @@ var MazeModel = Backbone.Model.extend({
 		currPos["x"] === 0 ? ++currPos["x"] : --currPos["x"];
 		currPos["y"] = Math.round(Math.random()) * (this.get("n") - 1);
 		currPos["y"] === 0 ? ++currPos["y"] : --currPos["y"];
+		//This will be the player's starting position
+		this.set("playerStart", [currPos["x"], currPos["y"]]);
 
 		//Visit the cell in the maze by marking it's value as 0
-		this.get("maze")[currPos["x"]][currPos["y"]] = -1;
+		this.get("maze")[currPos["x"]][currPos["y"]] = 0;
 		//Decrement the visited count
 		--visitedCount;
 
@@ -75,16 +77,16 @@ var MazeModel = Backbone.Model.extend({
 				var currNeighbors = [];
 
 				//Left neighbor
-				if(currPos["x"] - 2 > 0 && this.get("maze")[currPos["x"] - 2][currPos["y"]] === 0)
+				if(currPos["x"] - 2 > 0 && this.get("maze")[currPos["x"] - 2][currPos["y"]] === 1)
 					currNeighbors.push([currPos["x"] - 2, currPos["y"]]);
 				//Right neighbor
-				if(currPos["x"] + 2 < this.get("n") - 1 && this.get("maze")[currPos["x"] + 2][currPos["y"]] === 0)
+				if(currPos["x"] + 2 < this.get("n") - 1 && this.get("maze")[currPos["x"] + 2][currPos["y"]] === 1)
 					currNeighbors.push([currPos["x"] + 2, currPos["y"]]);
 				//Down neighbor
-				if(currPos["y"] - 2 > 0 && this.get("maze")[currPos["x"]][currPos["y"] - 2] === 0)
+				if(currPos["y"] - 2 > 0 && this.get("maze")[currPos["x"]][currPos["y"] - 2] === 1)
 					currNeighbors.push([currPos["x"], currPos["y"] - 2]);
 				//Up neighbor
-				if(currPos["y"] + 2 < this.get("n") - 1 && this.get("maze")[currPos["x"]][currPos["y"] + 2] === 0)
+				if(currPos["y"] + 2 < this.get("n") - 1 && this.get("maze")[currPos["x"]][currPos["y"] + 2] === 1)
 					currNeighbors.push([currPos["x"], currPos["y"] + 2]);
 
 				//If there were any unvistied neighbors retrieved
@@ -97,11 +99,11 @@ var MazeModel = Backbone.Model.extend({
 					//Remove wall between current cell and chosen cell
 					var wallOffsets = [(randomNeighbor[0] - currPos["x"])/2,
 						(randomNeighbor[1] - currPos["y"])/2];
-					this.get("maze")[currPos["x"] + wallOffsets[0]][currPos["y"] + wallOffsets[1]] = -1;
+					this.get("maze")[currPos["x"] + wallOffsets[0]][currPos["y"] + wallOffsets[1]] = " ";
 					//Make the chosen cell the current cell and mark as visited
 					currPos["x"] = randomNeighbor[0];
 					currPos["y"] = randomNeighbor[1];
-					this.get("maze")[currPos["x"]][currPos["y"]] = -1;
+					this.get("maze")[currPos["x"]][currPos["y"]] = 0;
 					--visitedCount;
 				}
 
@@ -117,19 +119,17 @@ var MazeModel = Backbone.Model.extend({
 			console.log(this.get("maze"));
 		// };
 
-		//Traverse maze
-		// traverseMaze();
+		//Set the player's starting position in the maze
+		this.get("maze")[this.get("playerStart")[0]][this.get("playerStart")[1]] = 2;
 	},
 
 	//A function to visually display the maze in the console
 	printMazeToConsole: function(){
 		var tmpString = "";
+		var horiWall = "_";
+		var vertWall = "|";
 		for(var i = 0; i < this.get("n"); ++i){
 			for(var j = 0; j < this.get("n"); ++j){
-				if(this.get("maze")[i][j] === -1){
-					tmpString += 0;
-					continue;
-				}
 				tmpString += this.get("maze")[i][j];
 			}
 			tmpString += "\n";
