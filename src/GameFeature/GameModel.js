0256102b21efab7afcd7cb2118f1b29fd9dfd17e
zfreedman <zfreedman92@gmail.com>
2015-09-05 16:47:20 -0700

    Implemented player movement for model in GameFeature/GameModel.js (accounts for out of bounds movement)

--- src/GameFeature/GameModel.js
+++ src/GameFeature/GameModel.js
@@ -16,6 +16,34 @@ var GameModel = Backbone.Model.extend({
     this.set("mazeModel", new MazeModel({n: 2*(this.get("currLevel") + 1) + 1}));
   },
 
+  //A function to move the player
+  //based on their current position
+  movePlayer: function(params){
+  	//Return false immedaitely if both parameters exceed a magnitude of 1
+  	if(Math.abs(params.direction[0]) > 0 && Math.abs(params.direction[1]) > 0)
+  		return false;
+  	//Get the player's position
+  	var playerPos = this.get("mazeModel").get("playerStart");
+  	console.log(playerPos);
+  	//Check if the direction is valid
+    //NOTE: valid if position + dir isn't equal to '|' or '-'
+    var newPlayerPos = [params.direction[0] + playerPos[0],
+    	params.direction[1] + playerPos[1]];
+    //Check if position in maze is valid
+    var mazePos = this.get("mazeModel").get("maze")[newPlayerPos[0]][newPlayerPos[1]]
+    if(mazePos !== "|" && mazePos != "-"){
+    	//Move is valid, move the player
+    	//Old position is empty space
+    	this.get("mazeModel").get("maze")[playerPos[0]][playerPos[1]] = " ";
+    	//New player pos is specified by a P
+    	this.get("mazeModel").get("maze")[newPlayerPos[0]][newPlayerPos[1]] = "P";
+    	this.get("mazeModel").set("playerStart", [newPlayerPos[0], newPlayerPos[1]]);
+    	return true;
+    }
+    //Otherwise player move failed
+    return false;
+  },
+
   //Render function
   render: function(){
 
